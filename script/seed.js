07aa9104793990366e492e1a6618be9b6e200d7c
captainozlem <ozlemniksch@gmail.com>
2019-01-08 10:55:21 -0600

    stock table updated

--- script/seed.js
+++ script/seed.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
-const db = require('../server/db')
-const { User } = require('../server/db/models')
-const { Stock } = require('../server/db/models')
+const db = require('../server/db');
+const {User} = require('../server/db/models');
+const {Stock} = require('../server/db/models');
 
-async function seed () {
-  await db.sync({ force: true })
-  console.log('db synced!')
+async function seed() {
+  await db.sync({force: true});
+  console.log('db synced!');
 
   const users = await Promise.all([
     User.create({
@@ -49,29 +49,129 @@ async function seed () {
       role: false,
       visits: 66
     })
-  ])
+  ]);
 
-  const stocks = await Promise.all([Stock.create({ name: 'Jelly Bean' })])
+  const stocks = Promise.all([
+    Stock.create({
+      name: 'Jelly Beans',
+      description: 'An American Classic',
+      images: '/images/jelly_beans.jpeg',
+      quantity: 1000,
+      price: 3.99,
+      category: 'Jelly Beans',
+      averageReview: 4.5,
+      brand: 'JB'
+    }),
+    Stock.create({
+      name: 'Cherry Candy',
+      description: 'Like a real cherry but better',
+      images: '/images/cherry_candy.jpeg',
+      quantity: 120,
+      price: 4.12,
+      category: 'Candy',
+      averageReview: 4.2,
+      brand: 'Candy Shop'
+    }),
+    Stock.create({
+      name: 'Gummy Bears',
+      description: 'Sweetest bears ever ',
+      images: '/images/gummy_bears.jpeg',
+      quantity: 100,
+      price: 2.49,
+      category: 'Jelly Beans',
+      averageReview: 4.5,
+      brand: 'Haribo'
+    }),
+    Stock.create({
+      name: 'Assorted Chocolate',
+      description: 'Perfect Halloween Treats',
+      images: '/images/halloweentreats.jpeg',
+      quantity: 1000,
+      price: 11.89,
+      category: 'Chocolate',
+      averageReview: 4.7,
+      brand: 'Nestle'
+    }),
+    Stock.create({
+      name: 'Hard Candies',
+      images: '/images/hardcandies.jpeg',
+      quantity: 40,
+      price: 6.8,
+      category: 'Candy',
+      averageReview: 4.2,
+      brand: 'Candy Shop'
+    }),
+    Stock.create({
+      name: 'Lollipops',
+      description: 'Yummy!',
+      images: '/images/lollipops.jpeg',
+      quantity: 700,
+      price: 1.99,
+      category: 'Candy',
+      averageReview: 5.0,
+      brand: 'Candy Shop'
+    }),
+    Stock.create({
+      name: 'M&M',
+      description: 'An American Classic',
+      images: '/images/mandm.jpeg',
+      quantity: 400,
+      price: 4.19,
+      category: 'Chocolate',
+      averageReview: 4.5,
+      brand: 'M&M'
+    }),
+    Stock.create({
+      name: 'Pinkle Candy',
+      description: 'A South Korean delicacy',
+      images: '/images/pinkle_candy.jpeg',
+      quantity: 10,
+      price: 10.99,
+      category: 'Exotic Deserts',
+      averageReview: 5.0,
+      brand: 'Pinkle'
+    }),
+    Stock.create({
+      name: 'Thai Sweets',
+      description: 'A Thai delicacy',
+      images: '/images/thai_sweets.jpeg',
+      quantity: 10,
+      price: 10.99,
+      category: 'Exotic Deserts',
+      averageReview: 5.0,
+      brand: 'Thai'
+    }),
+    Stock.create({
+      name: 'Turkish Delight',
+      description: 'A Turkish classic',
+      images: '/images/turkish_delight.jpeg',
+      quantity: 10,
+      price: 10.99,
+      category: 'Exotic Deserts',
+      averageReview: 5.0,
+      brand: 'Lokumcu Baba'
+    })
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded ${stocks.length} stocks`)
-  console.log(`seeded successfully`)
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded ${stocks.length} stocks`);
+  console.log(`seeded successfully`);
 }
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
 // The `seed` function is concerned only with modifying the database.
-async function runSeed () {
-  console.log('seeding...')
+async function runSeed() {
+  console.log('seeding...');
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log('closing db connection');
+    await db.close();
+    console.log('db connection closed');
   }
 }
 
@@ -79,8 +179,8 @@ async function runSeed () {
 // `Async` functions always return a promise, so we can use `catch` to handle
 // any errors that might occur inside of `seed`.
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
