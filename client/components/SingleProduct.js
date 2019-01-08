1ae288f13f2d5eab366f2204c0b98b62ef0f29b6
captainozlem <ozlemniksch@gmail.com>
2019-01-08 17:00:39 -0600

    Navbar corrected, will start Star rating...

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 mapStateToProps = state => {
-  return { products: state.stock.products }
-}
+  return {products: state.stock.products};
+};
 
-SingleProduct = props => {
-  const { product } = props
+export const SingleProduct = props => {
+  const {product} = props;
 
   return (
-    <div className='singleview'>
+    <div className="singleview">
       <img src={product.imageUrl} />
       {/* <Images images={product.images} */}
-      <div className='producttext'>
+      <div className="producttext">
         <h4>{product.description}</h4>
       </div>
       {/* <Stars rating={product.averageReview} */}
       <h3>${product.price}</h3>
     </div>
-  )
-}
+  );
+};
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,113 +1,148 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-<<<<<<< HEAD
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { logout } from '../store'
-
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to='/home'>Home</Link>
-          <a href='#' onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>{/* The navbar will show these links before you log in */}</div>
-      )}
-    </nav>
-=======
-import {connect} from 'react-redux'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 // import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import {logout} from '../store';
 
 const Navbar = () => (
-<div class="outline">
-  <div class="navRow">
-    <div><img src="./images/wLogo.png" class="wLogo" /></div>
-    <div class="dropdown1">
-      <button class="dropbtn1">FIND YOUR SWEETS</button>
-      <div class="dropdown-content1">
-        <ul>
-          <li></li>
-        </ul>
-        <div class="dropdown-list1">
+  <div className="outline">
+    <div className="navRow">
+      <div>
+        <img src="./images/wLogo.png" className="wLogo" />
+      </div>
+      <div className="dropdown1">
+        <button type="button" className="dropbtn1">
+          FIND YOUR SWEETS
+        </button>
+        <div className="dropdown-content1">
           <ul>
-            <li><a href="#">Candy</a></li>
-            <hr />
-            <li class="thin"><a href="#">New</a></li>
-            <li class="thin"><a href="#">Sale!</a></li>
-            <li class="thin"><a href="#">Lollipops</a></li>
-            <li class="thin"><a href="#">Gold Collections</a></li>
-            <li class="thin"><a href="#">Jelly Beans</a></li>
+            <li />
           </ul>
+          <div className="dropdown-list1">
+            <ul>
+              <li>
+                <a href="#">Candy</a>
+              </li>
+              <hr />
+              <li className="thin">
+                <a href="#">New</a>
+              </li>
+              <li className="thin">
+                <a href="#">Sale!</a>
+              </li>
+              <li className="thin">
+                <a href="#">Lollipops</a>
+              </li>
+              <li className="thin">
+                <a href="#">Gold Collections</a>
+              </li>
+              <li className="thin">
+                <a href="#">Jelly Beans</a>
+              </li>
+            </ul>
+          </div>
+          <div className="dropdown-list1">
+            <ul>
+              <li>
+                <a href="#">Gummy</a>
+              </li>
+              <hr />
+              <li className="thin">
+                <a href="#">Signature Filabelles</a>
+              </li>
+              <li className="thin">
+                <a href="#">Sour</a>
+              </li>
+              <li className="thin">
+                <a href="#">Novelty</a>
+              </li>
+              <li className="thin">
+                <a href="#">Rainbow</a>
+              </li>
+            </ul>
+          </div>
+          <div classNames="dropdown-list1">
+            <ul>
+              <li>
+                <a href="#">Chocolate</a>
+              </li>
+              <hr />
+              <li className="thin">
+                <a href="#">Chocolate Bar</a>
+              </li>
+              <li className="thin">
+                <a href="#">Choc-a-Lot</a>
+              </li>
+              <li className="thin">
+                <a href="#">Fudge</a>
+              </li>
+              <li className="thin">
+                <a href="#">Candy Cafe Bites</a>
+              </li>
+              <li className="thin">
+                <a href="#">Hand Dipped</a>
+              </li>
+            </ul>
+          </div>
+          <div className="dropdown-list1">
+            <ul>
+              <li>
+                <a href="#">Collections</a>
+              </li>
+              <hr />
+              <li className="thin">
+                <a href="#">Valentine's Day</a>
+              </li>
+              <li className="thin">
+                <a href="#">Chrismas</a>
+              </li>
+              <li className="thin">
+                <a href="#">City Collection</a>
+              </li>
+              <li className="thin">
+                <a href="#">Mean Girls</a>
+              </li>
+            </ul>
+          </div>
         </div>
-
-        <div class="dropdown-list1">
-          <ul>
-            <li><a href="#">Gummy</a></li>
-            <hr />
-            <li class="thin"><a href="#">Signature Filabelles</a></li>
-            <li class="thin"><a href="#">Sour</a></li>
-            <li class="thin"><a href="#">Novelty</a></li>
-            <li class="thin"><a href="#">Rainbow</a></li>
-          </ul>
+      </div>
+      <div className="dropdown2">
+        <button type="button" className="dropbtn2">
+          STORE LOCATION
+        </button>
+        <div className="dropdown-content2">
+          <a href="#">New York</a>
+          <a href="#">Chicago</a>
+          <a href="#">Miami</a>
+          <a href="#">Los Angeles</a>
         </div>
-        <div class="dropdown-list1">
-          <ul>
-            <li><a href="#">Chocolate</a></li>
-            <hr />
-            <li class="thin"><a href="#">Chocolate Bar</a></li>
-            <li class="thin"><a href="#">Choc-a-Lot</a></li>
-            <li class="thin"><a href="#">Fudge</a></li>
-            <li class="thin"><a href="#">Candy Cafe Bites</a></li>
-            <li class="thin"><a href="#">Hand Dipped</a></li>
-          </ul>
+      </div>
+      <div className="dropdown3">
+        <button type="button" className="dropbtn3">
+          ABOUT US
+        </button>
+        <div className="dropdown-content3">
+          <a href="#">About Us</a>
+          <a href="#">Events</a>
+          <a href="#">Inspiration</a>
         </div>
-        <div class="dropdown-list1">
-          <ul>
-            <li><a href="#">Collections</a></li>
-            <hr />
-            <li class="thin"><a href="#">Valentine's Day</a></li>
-            <li class="thin"><a href="#">Chrismas</a></li>
-            <li class="thin"><a href="#">City Collection</a></li>
-            <li class="thin"><a href="#">Mean Girls</a></li>
-          </ul>
+      </div>
+      <div className="dropdown4">
+        <button type="button" className="dropbtn4">
+          SIGN UP | LOGIN
+        </button>
+      </div>
+      <div className="dropdown5">
+        <button type="button" className="dropbtn5">
+          SHOPPING BAG
+        </button>
+        <div className="bag">
+          <img src="./images/shoppingBag.png" id="bag" />
         </div>
       </div>
     </div>
-    <div class="dropdown2">
-      <button class="dropbtn2">STORE LOCATION</button>
-      <div class="dropdown-content2">
-        <a href="#">New York</a>
-        <a href="#">Chicago</a>
-        <a href="#">Miami</a>
-        <a href="#">Los Angeles</a>
-      </div>
-    </div>
-    <div class="dropdown3">
-      <button class="dropbtn3">ABOUT US</button>
-      <div class="dropdown-content3">
-        <a href="#">About Us</a>
-        <a href="#">Events</a>
-        <a href="#">Inspiration</a>
-      </div>
-    </div>
-    <div class="dropdown4">
-      <button class="dropbtn4">SIGN UP | LOGIN</button>
-    </div>
-    <div class="dropdown5">
-      <button class="dropbtn5">SHOPPING BAG</button>
-      <div class="bag"><img src="./images/shoppingBag.png" id="bag" /></div>
-    </div>
->>>>>>> a78ef8508d409614c74515f0424496e28909f8af
   </div>
-</div>
-)
+);
 
 /**
  * CONTAINER
@@ -115,21 +150,18 @@ const Navbar = () => (
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
-  }
-}
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
-    handleClick () {
-      dispatch(logout())
+    handleClick() {
+      dispatch(logout());
     }
-  }
-}
+  };
+};
 
-export default connect(
-  mapState,
-  mapDispatch
-)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
 
 /**
  * PROP TYPES
@@ -137,4 +169,4 @@ export default connect(
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
