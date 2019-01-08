ddb0e32047230c5d70a078ccb8a6241a88af2608
Noah <noahbehtel@gmail.com>
2019-01-08 14:16:39 -0600

    admin page

--- client/components/SingleProduct-Admin.js
+++ client/components/SingleProduct-Admin.js
@@ -23,13 +23,68 @@ class SingleProduct extends Component {
   render () {
     return (
       <div className='singleview'>
-        <img src={product.imageUrl} />
-        {/* <Images images={product.images} */}
-        <div className='producttext'>
-          <h4>{product.description}</h4>
+        <form
+        onSubmit={
+          // thunk request for updating product
+        }
+        >
+          <h1>{product.name}</h1>
+          <input
+            className='input'
+            type='text'
+            onChange={evt => {
+              this.setState({
+                name: evt.target.value
+              })
+            }}
+            value={this.state.name}
+          />
+          <img src={product.imageUrl[1]} />
+          {/* <Images images={product.images} */}
+          <div className='producttext'>
+            <h4>{product.description}</h4>
+          </div>
+          <input
+            className='input'
+            type='text'
+            onChange={evt => {
+              this.setState({
+                description: evt.target.value
+              })
+            }}
+            value={this.state.description}
+          />
+          {/* <Stars rating={product.reviews} */}
+          <h3>${product.price}</h3>
+          <input
+            className='input'
+            type='number'
+            min='1'
+            step='any'
+            onChange={evt => {
+              this.setState({
+                price: evt.target.value
+              })
+            }}
+            value={this.state.price}
+          />
+          {/* THUNK>>>> <button onclick={addToCart(product.id)}>Add To Cart</button> */}
+          <h4>{product.quantity}</h4>
+          <input
+            className='input'
+            type='number'
+            onChange={evt => {
+              this.setState({
+                quantity: evt.target.value
+              })
+            }}
+            value={this.state.quantity}
+          />
+          <button type="submit">Save</button>
+        </form>
+        <div className='reviews'>
+          <Reviews product={product} />
         </div>
-        {/* <Stars rating={product.averageReview} */}
-        <h3>${product.price}</h3>
       </div>
     )
   }
--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -11,63 +11,13 @@ SingleProduct = props => {
 
   return (
     <div className='singleview'>
-      <form>
-        <h1>{product.name}</h1>
-        <input
-          className='input'
-          type='text'
-          onChange={evt => {
-            this.setState({
-              name: evt.target.value
-            })
-          }}
-          value={this.state.name}
-        />
-        <img src={product.imageUrl[1]} />
-        {/* <Images images={product.images} */}
-        <div className='producttext'>
-          <h4>{product.description}</h4>
-        </div>
-        <input
-          className='input'
-          type='text'
-          onChange={evt => {
-            this.setState({
-              description: evt.target.value
-            })
-          }}
-          value={this.state.description}
-        />
-        {/* <Stars rating={product.averageReview} */}
-        <h3>${product.price}</h3>
-        <input
-          className='input'
-          type='number'
-          min='1'
-          step='any'
-          onChange={evt => {
-            this.setState({
-              price: evt.target.value
-            })
-          }}
-          value={this.state.price}
-        />
-        {/* <button onclick={addToCart(product.id)}>Add To Cart</button> */}
-        <h4>{product.quantity}</h4>
-        <input
-          className='input'
-          type='number'
-          onChange={evt => {
-            this.setState({
-              quantity: evt.target.value
-            })
-          }}
-          value={this.state.quantity}
-        />
-      </form>
-      <div className='reviews'>
-        <Reviews product={product} />
+      <img src={product.imageUrl} />
+      {/* <Images images={product.images} */}
+      <div className='producttext'>
+        <h4>{product.description}</h4>
       </div>
+      {/* <Stars rating={product.averageReview} */}
+      <h3>${product.price}</h3>
     </div>
   )
 }
