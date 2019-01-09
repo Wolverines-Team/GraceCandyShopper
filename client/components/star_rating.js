4d043351d31f10422b9d51b929d1d98253272ee9
captainozlem <ozlemniksch@gmail.com>
2019-01-09 16:50:57 -0600

    empty cart view is working, cart in progress

--- client/components/star_rating.js
+++ client/components/star_rating.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 
-type Props = {};
-
 export default class StarRating extends Component {
+  constructor() {
+    super();
+    this.setState = {
+      ratings: 0,
+      views: 0
+    };
+  }
   render() {
-    return <div className="star_container" />;
+    return (
+      <div className="star_container">
+        <img className="star_image" src="../public/star_filled.png" />
+        <img className="star_image" src="../public/star_filled.png" />
+        <img className="star_image" src="../public/star_filled.png" />
+        <img className="star_image" src="../public/star_filled.png" />
+        <img className="star_image" src="../public/star_filled.png" />
+        <p className="star_text"> {this.state} </p>
+      </div>
+    );
   }
 }
