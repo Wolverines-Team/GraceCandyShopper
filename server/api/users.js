50040cc9c402a9b55ad20ef091fc0334d85de4b9
Edwin Kim <edwin.k.kim@gmail.com>
2019-01-09 10:50:51 -0600

    updated db models & routers

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User } = require('../db/models')
 module.exports = router
 
+//All users. (Edwin's Comment: for Admin's view??)
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -11,6 +12,53 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Single user (Edwin's comment: For signed in user view? If so, need to do something with authentication?)
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId)
+    res.status(200).json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Creating a new user (Edwin's Commenmt: Authentication??)
+router.post('/', async (req, res, next) => {
+  try {
+    //Edwin's Comment: Do we want to use the entire req.body form?
+    const newUser = await User.create(req.body)
+    res.status(200).json(newUser)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Update user (Edwin's Comment: Authentication?)
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.params.userId)
+    //Edwin's Comment: Do we want the entre req.body form?
+    const updatedUser = await currentUser.update(req.body)
+    res.status(200).json(updatedUser)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//Remove user (Edwin's comment: authentication? Admin?)
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.userId
+      }
+    })
+    res.sendStatus(200)
   } catch (err) {
     next(err)
   }
