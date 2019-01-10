ed25afa5fa439b72269b173b6a055200dc02b700
captainozlem <ozlemniksch@gmail.com>
2019-01-10 14:56:23 -0600

    cart is in progress

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+//"OZLEM`S NOTE"
+//GET_ITEM means products in the cart
+
 //action types
 const GET_ITEM = 'GET_ITEM';
 const ADD_ITEM = 'ADD_ITEM';
@@ -30,7 +33,7 @@ export const updateQuantity = item => ({
 //thunk creators
 export const fetchItems = () => async dispatch => {
   try {
-    const {data} = await axios.post('api/cart');
+    const { data } = await axios.get('/api/cart');
     dispatch(getItem(data));
   } catch (error) {
     console.log(error);
@@ -43,7 +46,7 @@ export const fetchItems = () => async dispatch => {
 
 export const postItems = newItem => async dispatch => {
   try {
-    const {data} = await axios.post(`/api/cart/${newItem.id}`, newItem);
+    const { data } = await axios.post(`/api/cart`, newItem);
     dispatch(addItem(data));
   } catch (error) {
     console.log(error);
@@ -60,7 +63,7 @@ export const deleteItems = itemId => async dispatch => {
 
 export const updateItemQuantity = item => async dispatch => {
   try {
-    const {data} = await axios.put(`/api/cart/${item.id}`, item.quantity);
+    const { data } = await axios.put(`/api/cart/${item.id}`, item.quantity);
     dispatch(updateQuantity(data));
   } catch (error) {
     console.error(error);
@@ -70,7 +73,7 @@ export const updateItemQuantity = item => async dispatch => {
 // Reducer
 const defaultState = [];
 
-export default function(cartState = defaultState, action) {
+export default function (cartState = defaultState, action) {
   switch (action.type) {
     case GET_ITEM:
       return action.items;
@@ -81,7 +84,7 @@ export default function(cartState = defaultState, action) {
     case UPDATE_QUANTITY:
       return cartState.map(item => {
         if (item.id === action.item.id) {
-          return {...item, ...action.item};
+          return { ...item, ...action.item };
         } else {
           return item;
         }
